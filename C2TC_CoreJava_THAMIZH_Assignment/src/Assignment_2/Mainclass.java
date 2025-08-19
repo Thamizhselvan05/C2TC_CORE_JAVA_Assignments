18bdc67abe277dea05c98bc2e64aaec7bf07809f
Thamizhselvan05 <gthamizhselvang@gmail.com>
2025-08-19 18:31:15 +0530

    Commit

---
+++ C2TC_CoreJava_THAMIZH_Assignment/src/Assignment_2/Mainclass.java
@@ -0,0 +1,15 @@
+package Assignment_2;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+  
+        Student student = new Student();
+
+       
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+    }
+
+}
